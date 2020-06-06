fbfa0f95906131f8d43533617f6709bd3cebc80b
Gus-oss <60494826+Gus-oss@users.noreply.github.com>
2020-06-05 21:32:19 -0500

    Programa_fibonacci y funciones

---
+++ Programa_ fibonacci y funciones/Program_Fibonacciorecursiva.cpp
@@ -0,0 +1,22 @@
+/*Librerias*/
+#include<stdio.h>
+/************************************************/
+int fibbonacci(int);
+int main()
+{
+
+	printf("Serie:%d",fibbonacci(50));
+	return 0;
+}
+
+int fibbonacci(int n) 
+{
+   if(n == 0){
+      return 0;
+   } else if(n == 1) {
+      return 1;
+   } else {
+      return (fibbonacci(n-1) + fibbonacci(n-2));
+   }
+}
+/************************************************(
